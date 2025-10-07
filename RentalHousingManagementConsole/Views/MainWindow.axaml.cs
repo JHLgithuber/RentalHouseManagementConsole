f55afdc9036f55a543c435adccd3b76c44810a18
LeeJinHyeong <jfttstudio@gmail.com>
2025-10-07 16:17:43 +0900

    프로젝트 초기 세팅 추가: `.gitignore` 파일 생성, Android/iOS/Browser 메인 설정 및 리소스 파일 추가, Avalonia 초기화 작업 설정.

---
+++ RentalHousingManagementConsole/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace RentalHousingManagementConsole.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
